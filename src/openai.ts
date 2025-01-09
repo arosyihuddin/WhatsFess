00aa2a834be7f211788675ed79a0a6c63fe9c4b8
arosyihuddin <arosyihuddin6@gmail.com>
2025-01-10 01:06:36 +0700

    update package.json

--- src/openai.ts
+++ src/openai.ts
@@ -14,7 +14,7 @@ export const openaiChat = async (message: string) => {
             { role: "system", content: "Kamu adalah WhatsFess, Asisten Yang sangat Membantu." },
             { role: "system", content: "Kamu dapat mengirimkan pesan ke nomor WhatsApp yang pengguna inginkan." },
             { role: "system", content: "Kamu dapat mengirimkan pesan ke channel WhatsFess Secara Anonym (Confess)" },
-            { role: "system", content: "Cara Mengirim Pesan Ke Channel WhatsFess adalah dengan mengirim pesan ke nomor https://wa.me/6289518291377 dan menggunakan format seperti ini '@menfess \n<pesan>\n#nama Samaran (Anonym)', contoh: @menfess \nHalo Semuanya.. \n#MiminWhatsFess" },
+            { role: "system", content: "Cara Mengirim Pesan Ke Channel WhatsFess adalah dengan mengirim pesan ke nomor https://wa.me/6289518291377 dan menggunakan format seperti ini '@menfess \n<pesan>\n#nama Samaran (Anonym)', contoh: @menfess \nHalo Semuanya.. \n#MiminWhatsFess setelah itu kirim ke nomor ini https://wa.me/6289518291377" },
             { role: "system", content: "Link Chnnel WhatsFess https://whatsapp.com/channel/0029Vb2SPkC6LwHr0IGsFR2h" },
             { role: "user", content: message }
         ],
